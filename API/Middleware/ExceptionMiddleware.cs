5560a38970a1ffd29ae7123132c3489eb21dfa78
sebastraszewski <seba.straszewski@gmail.com>
2023-06-05 19:57:51 +0200

    end 5

---
+++ API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,48 @@
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using API.Errors;
+using SQLitePCL;
+
+namespace API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _env;
+        public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger, IHostEnvironment env)
+        {
+            _env = env;
+            _logger = logger;
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (System.Exception ex)
+            {
+                
+                _logger.LogError(ex, ex.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                
+                var response = _env.IsDevelopment()
+                    ? new ApiExeption((int)HttpStatusCode.InternalServerError, ex.Message, ex.StackTrace.ToString())
+                    : new ApiExeption((int)HttpStatusCode.InternalServerError);
+
+                var options = new JsonSerializerOptions{PropertyNamingPolicy = JsonNamingPolicy.CamelCase};
+
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
+            }
+
+
+        }
+    }
+}
